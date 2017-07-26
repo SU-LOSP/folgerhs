c3a1dd7c56f3260892f7d47bdc10e2fd67cf6030
Unai Zalakain <unai@gisa-elkartea.org>
2017-07-26 11:52:43 +0200

    Sort speakers in descending order

--- app/Folgerhs/Speakers.hs
+++ app/Folgerhs/Speakers.hs
@@ -3,6 +3,7 @@ module Folgerhs.Speakers (speakers) where
 import Data.Function (on)
 import Data.List
 import Data.Maybe
+import Data.Ord
 
 import Text.Printf (printf)
 
@@ -12,7 +13,7 @@ import Folgerhs.Parse (parse)
 
 sortedPercent :: [Character] -> [(Character, Float)]
 sortedPercent chs = let perCh = [ (head g, g % chs) | g <- group (sort chs) ]
-                     in sortBy (on compare snd) perCh
+                     in sortBy (flip $ comparing snd) perCh
                     where (%) a b = on (/) (fromIntegral . length) a b
                             
 reprProt :: (Character, Float) -> String
